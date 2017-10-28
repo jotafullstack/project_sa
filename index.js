b96a45ef225caee716a8f5bfaf8b104fafc8b722
Jean-Pierre Carvalho <jeanpierre.carvalho@solfut.com>
2017-10-28 12:07:11 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.use(express.static('public'));
+
+app.use('/app', express.static(path.join(__dirname, 'app')))
+
+
+//app.use(express.static(__dirname + '/public'));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
